0885cd3ae3c1e8bab48245b87c68acc37a8b8103
Cole <cmasoninbox@gmail.com>
2026-08-09 13:26:08 +1000

    Update Firefox extension filename to v1.1.1

--- server.js
+++ server.js
@@ -2103,7 +2103,7 @@ app.get('/extension/:browser', (req, res, next) => {
   const sourceBrowser = browser === 'firefox' ? 'firefox' : 'chrome';
   const sourceDir = path.join(__dirname, 'extension', sourceBrowser);
   const filename = browser === 'firefox'
-    ? 'pvpn-firefox-extension-v1.1.0.zip'
+    ? 'pvpn-firefox-extension-v1.1.1.zip'
     : 'pvpn-' + browser + '-extension-v1.1.0.zip';
 
   res.attachment(filename);
